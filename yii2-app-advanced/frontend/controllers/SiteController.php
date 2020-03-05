fdbf741414b31cc0dde900f9515943b0d04e464f
marco <masfasfarco>
2020-03-05 11:29:28 -0600

    Capitulo 7 Helpers terminado

--- yii2-app-advanced/frontend/controllers/SiteController.php
+++ yii2-app-advanced/frontend/controllers/SiteController.php
@@ -60,10 +60,7 @@ class SiteController extends Controller
             'error' => [
                 'class' => 'yii\web\ErrorAction',
             ],
-            'captcha' => [
-                'class' => 'yii\captcha\CaptchaAction',
-                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
-            ],
+            
         ];
     }
 
